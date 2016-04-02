7a862799cb4c4ace2431c804ef05c3ff4b9c7290
Carl <carl@carlossus.com>
2016-04-02 20:27:37 +0800

    Remove Phaser module loading reference from engine module

--- src/engine.ts
+++ src/engine.ts
@@ -1,21 +1,25 @@
-import * as Phaser from 'phaser';
+// Phaser is available globally via index.html
 
-const engine = new Phaser.Game(800, 600, Phaser.AUTO, 'content', {
-    create,
-    preload,
-    update
-});
-
-function preload() {
-    engine.load.image('terrain', 'assets/terrian_atlas.png');
+/**
+ * Singleton.. Why the hell would we want multiple of this monstrosity
+ */
+export default class Engine {
+    
+    static game = new Phaser.Game(800, 600, Phaser.AUTO, 'content',  {
+        preload: Engine.preload,
+        create: Engine.create,
+        update: Engine.update
+    });
+    
+    static preload() {
+        Engine.game.load.image('terrain', 'assets/terrain_atlas.png');
+    }
+    
+    static create() {
+        Engine.game.math.snapToFloor(1, 32);
+    }
+    
+    static update() {
+        
+    }
 }
-
-function create() {
-
-}
-
-function update() {
-
-}
-
-export { engine as default }
